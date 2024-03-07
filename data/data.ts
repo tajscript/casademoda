4386a54cf2c71e97d08d53d12b4ef7b279b6f4e5
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2024-03-07 18:32:37 +0100

    feat: Add hardcoded data

---
+++ data/data.ts
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 2,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 3,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 4,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 5,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 6,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 7,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 8,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 9,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 10,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 11,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+    {
+        id: 12,
+        title: "NIKE DUNK LOW ANTHRACITE",
+        amount: "389,999",
+        image: "/featured.jpg"
+    },
+]
